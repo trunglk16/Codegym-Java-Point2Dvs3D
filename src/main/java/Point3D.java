0768fddc07f2e45b9eda63ee8b21fbb9b73fda22
DOTHAIHUNG <44601578+DOTHAIHUNG@users.noreply.github.com>
2018-12-17 21:06:57 +0700

    Update Point3D.java
    da xoa nhung khoang trong thua va sua ten mang cuc bo

--- src/main/java/Point3D.java
+++ src/main/java/Point3D.java
@@ -4,7 +4,6 @@ public class Point3D extends Point2D{
     float z = 0.0f;
 
     public Point3D() {
-
     }
 
     public Point3D(float x, float y, float z) {
@@ -16,8 +15,6 @@ public class Point3D extends Point2D{
         return z;
     }
 
-
-
     public void setXYZ(float x, float y, float z){
         x = super.getX();
         y = super.getY();
@@ -25,11 +22,11 @@ public class Point3D extends Point2D{
     }
 
     public float[] getXYZ(){
-       float[] arr = new float[3] ;
-       arr[0] = super.getX();
-       arr[1] = super.getY();
-       arr[2] = this.z;
-        return arr;
+       float[] arrayXYZ = new float[3] ;
+       arrayXYZ[0] = super.getX();
+       arrayXYZ[1] = super.getY();
+       arrayXYZ[2] = this.z;
+        return arrayXYZ;
     }
 
     @Override
